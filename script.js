6d662babb2ecf22505a019a5092a65aa57bdee42
techinezz <y.martinez.tech@gmail.com>
2023-12-31 16:54:48 -0500

    Display Rank

--- script.js
+++ script.js
@@ -54,14 +54,26 @@ async function data() {
     
     // Ranked data
     const { tier: tier1, rank: rank1, wins: wins1, losses: losses1, queueType: queueType1, leaguePoints: leaguePoints1 } = rankedData[0];
-    document.getElementById("summoner_ranked_data").innerHTML = `Tier: ${tier1}, Rank: ${rank1}, Wins: ${wins1}, Losses: ${losses1}, Queue Type: ${queueType1}, LP: ${leaguePoints1}`;
+    document.getElementById("summoner_ranked_data1").innerHTML = `Tier: ${tier1}, Rank: ${rank1}, Wins: ${wins1}, Losses: ${losses1}, Queue Type: ${queueType1}, LP: ${leaguePoints1}`;
     
     const { tier: tier2, rank: rank2, wins: wins2, losses: losses2, queueType: queueType2, leaguePoints: leaguePoints2 } = rankedData[1];
-    document.getElementById("summoner_ranked_data").innerHTML += `<br>Tier: ${tier2}, Rank: ${rank2}, Wins: ${wins2}, Losses: ${losses2}, Queue Type: ${queueType2}, LP: ${leaguePoints2}`;
+    document.getElementById("summoner_ranked_data2").innerHTML += `<br>Tier: ${tier2}, Rank: ${rank2}, Wins: ${wins2}, Losses: ${losses2}, Queue Type: ${queueType2}, LP: ${leaguePoints2}`;
 
-    // Set rank image
-    var rankImageElement = document.getElementById("rank_image");
-    var rankImagePath = "rankimages/" + tier.toLowerCase() + ".png"; // assuming the image file names are the lowercased tier names
+    //Rank Image for Ranked Solo
+    var rankImageElement = document.getElementById("rank_image1");
+    var rankImagePath = "rankimages/" + tier1.toLowerCase() + ".png";
     rankImageElement.src = rankImagePath;
+
+    //Rank Image for Ranked Flex
+    var rankImageElement = document.getElementById("rank_image2");
+    var rankImagePath = "rankimages/" + tier2.toLowerCase() + ".png";
+    rankImageElement.src = rankImagePath;
+
+    //Show rank containers
+    const rankContainers = document.querySelectorAll('.rank-container');
+    for (const container of rankContainers) {
+        container.style.display = 'block';
+    }
+
 
 }
